bc85e2b249bb733b7fef1b5889e6c46efa124f26
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-11-22 09:58:08 -0600

    fdsa

---
+++ Cookies_local_storage/src/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function setCookies() {
+  var firstname = document.getElementById('firstname').value;
+  var email = document.getElementById('email').value;
+  document.cookie = 'firstname=' + encodeURIComponent(firstname);
+  document.cookie = 'email=' + encodeURIComponent(email);
+  alert('Welcome ' + firstname + ', have some cookies!');
+}
